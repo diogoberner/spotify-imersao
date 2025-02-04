a617cca59b4a29fa27c4cdc10c4539eb595533c8
Diogo CS Berner <diogoberner@gmail.com>
2025-02-04 19:45:45 -0300

    search function working

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const searchInput = document.getElementById("search-input")
 const resultPlaylists = document.getElementById("result-playlists")
 const resultArtist = document.getElementById("result-artist")
+const gridContainer = document.querySelector(".grid-container")
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
@@ -11,9 +12,28 @@ function requestApi(searchTerm) {
 
 function displayArtist(result) {
 
+    let artistCard = ""
     if (result.length > 0) {
         for (const artist of result) {
+            artistCard += `
+            <div class="artist-card">
+                        <div class="card-img">
+                            <img src="${artist.urlImg}" class="artist-img" id="artist-img">
+                            <div class="play">
+                                <span class="fa fa-solid fa-play"></span>
+                            </div>
+                        </div>
+                        <div class="card-text">
+                            <a href="" class="vst" title="${artist.name}"></a>
+                            <span class="artist-name" id="artist-name">${artist.name}</span>
+                            <span class="artist-category">${artist.genre}</span>
+                        </div>
+                    </div>
+            `
 
+            gridContainer.innerHTML = artistCard
+            resultPlaylists.classList.add("hidden")
+            resultArtist.classList.remove("hidden")
         }
     }
 
@@ -22,7 +42,7 @@ function displayArtist(result) {
 searchInput.addEventListener("input", (e) => {
     e.preventDefault()
 
-    const searchTerm = searchInput.value.toLowerCase()
+    const searchTerm = searchInput.value.trim().toLowerCase()
 
     if (searchTerm === "") {
         resultPlaylists.classList.remove("hidden")
@@ -30,7 +50,6 @@ searchInput.addEventListener("input", (e) => {
         return
     }
 
-    if (searchTerm !== "") {
-        requestApi(searchTerm)
-    }
+    requestApi(searchTerm)
+
 })
